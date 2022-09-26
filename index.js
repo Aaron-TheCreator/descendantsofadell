ffcba2afb22ae97b0ce5ae033b35415491f4576a
Aaaron Burns <a.aburns95@gmail.com>
2022-09-26 10:50:49 -0700

    forgot, but def added a deploy badge for readme

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function copy() {
-    // website address
-    const webAddress = "https://descendantsofadell.netlify.app"
-  
-    // // Select the text field
-    // copyText.select();
-    // copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(webAddress);
-    
-  
-    // Alert the copied text
-    alert("Copied the text: " + webAddress);
-  }
+// // website address
+// const webAddress = "https://descendantsofadell.netlify.app"
+let copyText = document.getElementById("copyText");
+
+// Select the text field
+copyText.select();
+copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+navigator.clipboard.writeText(copyText.value);
+
+
+// Alert the copied text
+alert("Copied the text: " + copyText.value);
+}
+
+
